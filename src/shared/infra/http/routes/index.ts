a63d2eed3c851a1dd552bd16dbccf2cbaf590724
joao.rezende <joaorabello99@gmail.com>
2021-03-28 01:16:47 -0300

    feat: adapt routering to new context

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
-import usersAdminRoutes from '@modules/users/infra/http/routes/usersAdmin.routes';
-import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import ensureAdmin from '@modules/users/infra/http/middlewares/ensureAdmin';
+import usersContractorRouter from '@modules/users/infra/http/routes/usersContract.routes';
+import usersTeacherRouter from '@modules/users/infra/http/routes/usersTeacher.routes';
+
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/api/users', usersRouter);
-routes.use('/api/admin', ensureAuthenticated, ensureAdmin, usersAdminRoutes);
+routes.use('/api/teachers', usersTeacherRouter);
+routes.use('/api/contractors', usersContractorRouter);
 routes.use('/api/sessions', sessionsRouter);
 routes.use('/api/password', passwordRouter);
 
