26bd9ece52371cc1966544e3ef09f1cb19f3f8b2
joao.rezende <joaorabello99@gmail.com>
2021-03-31 15:22:35 -0300

    feat: implements chat

--- src/modules/chats/services/SendMessageService.ts
+++ src/modules/chats/services/SendMessageService.ts
@@ -19,8 +19,82 @@ export default class SendMessageService {
       throw new AppError(`You can't send message to yourself`);
     }
 
-    console.log('message', message);
-    console.log('send_by', send_by);
-    console.log('send_to', send_to);
+    let has_chat_for_recipient = await this.chatRepository.findByUserId(send_to);
+    let has_chat_for_sender = await this.chatRepository.findByUserId(send_by);
+
+    if (!has_chat_for_recipient) {
+      has_chat_for_recipient = await this.chatRepository.create({
+        chats: [],
+        user_id: send_to,
+      });
+    }
+
+    if (!has_chat_for_sender) {
+      has_chat_for_sender = await this.chatRepository.create({
+        chats: [],
+        user_id: send_by,
+      });
+    }
+
+    const conversation_between_recipient_and_sender_index = has_chat_for_recipient.chats.findIndex(
+      item => item.user_id === send_by,
+    );
+
+    const conversation_between_sender_and_recipient_index = has_chat_for_sender.chats.findIndex(
+      item => item.user_id === send_to,
+    );
+
+    if (conversation_between_recipient_and_sender_index >= 0) {
+      const { messages } = has_chat_for_recipient.chats[
+        conversation_between_recipient_and_sender_index
+      ];
+
+      messages.push({
+        action: 'RECEIVED',
+        message,
+      });
+
+      has_chat_for_recipient.chats[
+        conversation_between_recipient_and_sender_index
+      ].messages = messages;
+    } else {
+      has_chat_for_recipient.chats.push({
+        user_id: send_by,
+        messages: [
+          {
+            action: 'RECEIVED',
+            message,
+          },
+        ],
+      });
+    }
+
+    if (conversation_between_sender_and_recipient_index >= 0) {
+      const { messages } = has_chat_for_sender.chats[
+        conversation_between_sender_and_recipient_index
+      ];
+
+      messages.push({
+        action: 'SENT',
+        message,
+      });
+
+      has_chat_for_sender.chats[
+        conversation_between_sender_and_recipient_index
+      ].messages = messages;
+    } else {
+      has_chat_for_sender.chats.push({
+        user_id: send_to,
+        messages: [
+          {
+            action: 'SENT',
+            message,
+          },
+        ],
+      });
+    }
+
+    await this.chatRepository.save(has_chat_for_recipient);
+    await this.chatRepository.save(has_chat_for_sender);
   }
 }
