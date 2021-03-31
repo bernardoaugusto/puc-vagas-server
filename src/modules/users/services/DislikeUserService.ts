473e7746cd4591c3313c3cd0ea4ecce8a5591c62
joao.rezende <joaorabello99@gmail.com>
2021-03-30 21:32:13 -0300

    feat: implements like and dislike on user

---
+++ src/modules/users/services/DislikeUserService.ts
@@ -0,0 +1,68 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IVacancyRepositoryDTO from '@modules/vacancies/repositories/IVacancyRepositoryDTO';
+import IVacancyLikeDislikeRepository from '@modules/vacancies/repositories/IVacancyLikeDislikeRepository';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+@injectable()
+export default class DislikeUserService {
+  constructor(
+    @inject('VacancyRepository')
+    private vacancyRepository: IVacancyRepositoryDTO,
+
+    @inject('VacancyLikeDislikeRepository')
+    private vacancyLikeDislikeRepository: IVacancyLikeDislikeRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute(
+    user_id: string,
+    vacancy_id: string,
+    user_disliked_id: string,
+  ): Promise<void> {
+    if (user_id === user_disliked_id) {
+      throw new AppError(`You can't like yourself`);
+    }
+
+    const vacancy_exists = await this.vacancyRepository.findById(vacancy_id);
+
+    if (!vacancy_exists) {
+      throw new AppError('Vacancy not found');
+    }
+
+    const check_user_liked_exists = await this.usersRepository.findById(
+      user_disliked_id,
+    );
+
+    if (!check_user_liked_exists) {
+      throw new AppError('User not found');
+    }
+
+    let has_register_of_vacancy = await this.vacancyLikeDislikeRepository.findByVacancyId(
+      vacancy_id,
+    );
+
+    if (!has_register_of_vacancy) {
+      has_register_of_vacancy = await this.vacancyLikeDislikeRepository.create({
+        vacancy_id,
+        likes: [],
+        dislikes: [],
+        matches: [],
+      });
+    }
+
+    if (
+      has_register_of_vacancy.likes.includes(vacancy_id) ||
+      has_register_of_vacancy.dislikes.includes(vacancy_id)
+    ) {
+      throw new AppError(`You already reacted to this user`);
+    }
+
+    has_register_of_vacancy.dislikes.push(check_user_liked_exists.id);
+
+    await this.vacancyLikeDislikeRepository.update(has_register_of_vacancy);
+  }
+}
