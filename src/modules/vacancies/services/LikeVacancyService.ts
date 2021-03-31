ef5978270f1c2b662ba0689ef50d15ca89c1009d
joao.rezende <joaorabello99@gmail.com>
2021-03-30 21:54:09 -0300

    fix: fix conflict

--- src/modules/vacancies/services/LikeVacancyService.ts
+++ src/modules/vacancies/services/LikeVacancyService.ts
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-import { inject, injectable } from 'tsyringe';
-
-import AppError from '@shared/errors/AppError';
-import IUserLikeDislikeRepository from '@modules/users/repositories/IUserLikeDislikeRepository';
-import IVacancyRepositoryDTO from '../repositories/IVacancyRepositoryDTO';
-
-@injectable()
-export default class LikeVacancyService {
-  constructor(
-    @inject('VacancyRepository')
-    private vacancyRepository: IVacancyRepositoryDTO,
-
-    @inject('UserLikeDislikeRepository')
-    private userLikeDislikeRepository: IUserLikeDislikeRepository,
-  ) {}
-
-  public async execute(user_id: string, vacancy_id: string): Promise<void> {
-    const vacancy_exists = await this.vacancyRepository.findById(vacancy_id);
-
-    if (!vacancy_exists) {
-      throw new AppError('Vacancy not found');
-    }
-
-    let has_register_of_user = await this.userLikeDislikeRepository.findByUserId(
-      user_id,
-    );
-
-    if (!has_register_of_user) {
-      has_register_of_user = await this.userLikeDislikeRepository.create({
-        user_id,
-        likes: [],
-        dislikes: [],
-        matches: [],
-      });
-    }
-
-    if (
-      has_register_of_user.likes.includes(vacancy_id) ||
-      has_register_of_user.dislikes.includes(vacancy_id)
-    ) {
-      throw new AppError(`You already reacted to this vacancy`);
-    }
-
-    has_register_of_user.likes.push(vacancy_exists.id);
-
-    await this.userLikeDislikeRepository.update(has_register_of_user);
-  }
-}
-=======
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -119,4 +69,3 @@ export default class LikeVacancyService {
     await this.userLikeDislikeRepository.update(has_register_of_user);
   }
 }
->>>>>>> feature/implements-like-dislike-vacancy
