ce3f8bf97fee9832b27e8fe1a398bb6b971776f9
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-04-23 13:33:48 -0300

    feat: add cascade delete vacancies

--- src/modules/vacancies/services/RemoveVacancyService.ts
+++ src/modules/vacancies/services/RemoveVacancyService.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import IHardSkillsRepositoryDTO from '@modules/hardSkills/repositories/IHardSkillsRepositoryDTO';
+import IVacancySoftSkillsRepositoryDTO from '@modules/vacancySoftSkills/repositories/IVacancySoftSkillsRepositoryDTO';
 import { inject, injectable } from 'tsyringe';
 
 import IVacancyRepositoryDTO from '../repositories/IVacancyRepositoryDTO';
@@ -11,10 +14,32 @@ export default class RemoveVacancyService {
 
     @inject('GetByIdVacancyService')
     private getByIdVacancyService: GetByIdVacancyService,
+
+    @inject('VacancySoftSkillsRepository')
+    private vacancySoftSkillsRepository: IVacancySoftSkillsRepositoryDTO,
+
+    @inject('HardSkillsRepository')
+    private hardSkillsRepository: IHardSkillsRepositoryDTO,
   ) {}
 
   public async execute(vacancyId: string): Promise<void> {
     await this.getByIdVacancyService.execute(vacancyId);
+
+    const vacancySoftSkills = await this.vacancySoftSkillsRepository.getAllWithoutPagination(
+      <any>{ where: { vacancy_id: vacancyId } },
+    );
+
+    for (const vacancySoftSkill of vacancySoftSkills) {
+      await this.vacancySoftSkillsRepository.remove(vacancySoftSkill.id);
+    }
+
+    const hardSkills = await this.hardSkillsRepository.getAllWithoutPagination(<any>{
+      where: { vacancy_id: vacancyId },
+    });
+
+    for (const hardSkill of hardSkills) {
+      await this.hardSkillsRepository.remove(hardSkill.id);
+    }
 
     await this.vacancyRepository.remove(vacancyId);
   }
