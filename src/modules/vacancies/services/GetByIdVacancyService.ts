4a690ad68a9c1f9f16ecce15bbcb38bcaaf66a24
joao.rezende <joaorabello99@gmail.com>
2021-05-10 20:46:16 -0300

    feat: implements like/dislike/match

--- src/modules/vacancies/services/GetByIdVacancyService.ts
+++ src/modules/vacancies/services/GetByIdVacancyService.ts
@@ -2,20 +2,49 @@ import { inject, injectable } from 'tsyringe';
 import AppError from '../../../shared/errors/AppError';
 
 import Vacancy from '../infra/typeorm/entities/Vacancy';
+import IVacancyLikeDislikeRepository from '../repositories/IVacancyLikeDislikeRepository';
 import IVacancyRepositoryDTO from '../repositories/IVacancyRepositoryDTO';
+
+interface Response {
+  vacancy: Vacancy;
+  likes: Array<string>;
+  dislikes: Array<string>;
+  matches: Array<string>;
+}
 
 @injectable()
 export default class GetByIdVacancyService {
   constructor(
     @inject('VacancyRepository')
     private vacancyRepository: IVacancyRepositoryDTO,
+
+    @inject('VacancyLikeDislikeRepository')
+    private vacancyLikeDislikeRepository: IVacancyLikeDislikeRepository,
   ) {}
 
-  public async execute(id: string): Promise<Vacancy> {
+  public async execute(id: string): Promise<Response> {
     const findedVacancy = await this.vacancyRepository.findById(id);
 
     if (!findedVacancy) throw new AppError('Vacancy not found', 404);
 
-    return findedVacancy;
+    const has_register_of_vacancy = await this.vacancyLikeDislikeRepository.findByVacancyId(
+      id,
+    );
+
+    if (!has_register_of_vacancy) {
+      return {
+        vacancy: findedVacancy,
+        dislikes: [],
+        likes: [],
+        matches: [],
+      };
+    }
+
+    return {
+      vacancy: findedVacancy,
+      dislikes: has_register_of_vacancy.dislikes,
+      likes: has_register_of_vacancy.likes,
+      matches: has_register_of_vacancy.matches,
+    };
   }
 }
