bcea248a497c3b4430a0f46e7c639ebda23306a0
Bernardo Lopes <bernardobpl97@gmail.com>
2021-06-10 01:56:45 -0300

    GetUsers para like agora também traz informações do mongo

--- src/modules/users/services/GetAllUsersForVacancy.ts
+++ src/modules/users/services/GetAllUsersForVacancy.ts
@@ -1,6 +1,7 @@
 import IVacancyLikeDislikeRepository from '@modules/vacancies/repositories/IVacancyLikeDislikeRepository';
 import { inject, injectable } from 'tsyringe';
 import User from '../infra/typeorm/entities/User';
+import IUserLikeDislikeRepository from '../repositories/IUserLikeDislikeRepository';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 @injectable()
@@ -11,6 +12,9 @@ export default class GetAllUsersForVacancy {
 
     @inject('VacancyLikeDislikeRepository')
     private vacancyLikeDislikeRepository: IVacancyLikeDislikeRepository,
+
+    @inject('UserLikeDislikeRepository')
+    private userLikeDislikeRepository: IUserLikeDislikeRepository,
   ) {}
 
   public async execute(user_id: string, vacancy_id: string): Promise<Array<User>> {
@@ -28,6 +32,32 @@ export default class GetAllUsersForVacancy {
 
     excluded_ids.push(user_id);
 
-    return this.usersRepository.getAllUsersForLikeOrDislike(excluded_ids);
+    const users = await this.usersRepository.getAllUsersForLikeOrDislike(excluded_ids);
+
+    const extra_info_for_users = await this.userLikeDislikeRepository.getAll();
+
+    const complete_users_array = users.map(user => {
+      const exist_extra_user_info = extra_info_for_users?.find(extraUserInfo => extraUserInfo.user_id === user.id);
+      if(exist_extra_user_info){
+        const { dislikes, matches, likes, recommendations } = exist_extra_user_info
+        return {
+          ...user,
+          dislikes,
+          matches,
+          likes,
+          recommendations,
+        }
+      } else {
+        return {
+          ...user,
+          recommendations: [],
+          dislikes: [],
+          likes: [],
+          matches: [],
+        }
+      }
+    })
+
+    return complete_users_array;
   }
 }
